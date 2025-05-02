20f1f502d73f679d940196ddbbd9ffc2fd8919ae
HoangHieu <hoangminhhieu230805@gmail.com>
2025-05-03 01:23:40 +0700

    Implement CORS configuration and enhance AuthResponse with additional user details

--- auction-frontend/src/services/api.js
+++ auction-frontend/src/services/api.js
@@ -5,30 +5,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    withCredentials: true, // Send cookies with requests
 });
-
-// Gắn JWT vào mọi request
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('jwt');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-// Xử lý lỗi 401 (token hết hạn, sai token, chưa login)
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            localStorage.removeItem('jwt'); 
-            window.location.href = '/login'; 
-        }
-        return Promise.reject(error);
-    }
-);
 
 export default api;
