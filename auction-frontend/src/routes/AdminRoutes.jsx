a73fc863df221b12c68479938ff6423e3260b736
Đăng <cckhongtroi@gmail.com>
2025-05-13 23:20:41 +0700

    category admin

--- auction-frontend/src/routes/AdminRoutes.jsx
+++ auction-frontend/src/routes/AdminRoutes.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/admin/Dashboard.jsx';
 import Auctions from '../pages/admin/Auctions.jsx';
 import AuctionDetail from '../pages/admin/AuctionDetail.jsx';
+import Categories from '../pages/admin/Categories-list.jsx';
 
 function AdminRoutes() {
 	return (
@@ -11,6 +12,7 @@ function AdminRoutes() {
 			<Route path="/dashboard" element={<Dashboard />} />
 			<Route path="/auctions" element={<Auctions />} />
 			<Route path="/auction/:id" element={<AuctionDetail />} />
+			<Route path="/categories-list" element={<Categories />} />
 		</Routes>
 	);
 }
