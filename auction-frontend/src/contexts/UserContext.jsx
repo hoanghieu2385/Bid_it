75034faf9b094079893a4886bf11fa5f99addebe
HoangHieu <hoangminhhieu230805@gmail.com>
2025-05-14 22:52:01 +0700

    fix lỗi bị hiển thị login, register trên header dù đã đăng nhập

--- auction-frontend/src/contexts/UserContext.jsx
+++ auction-frontend/src/contexts/UserContext.jsx
@@ -1,15 +1,30 @@
 // src/contexts/UserContext.jsx
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+import { getCurrentUser } from '../services/user-api';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
 
-	const loginUser = (userData) => {
-		setUser(userData);
-	};
+	useEffect(() => {
+		const fetchUser = async () => {
+			const token = localStorage.getItem('jwt');
+			if (token) {
+				try {
+					const userData = await getCurrentUser();
+					setUser(userData);
+				} catch (error) {
+					console.error('Error fetching user:', error);
+					localStorage.removeItem('jwt'); // Clear token nếu lỗi (ví dụ hết hạn)
+				}
+			}
+		};
 
+		fetchUser();
+	}, []);
+
+	const loginUser = (userData) => setUser(userData);
 	const logoutUser = () => {
 		localStorage.removeItem('jwt');
 		setUser(null);
--- auction-frontend/src/services/apiConfig.js
+++ auction-frontend/src/services/apiConfig.js
@@ -1,6 +1,7 @@
 // src/config/apiConfig.js
 const API_CONFIG = {
-    USER_SERVICE: '/user-service/auth',
+    USER_AUTH: '/user-service/auth',
+    USER_API: '/user-service/api/users',
     ORDER_SERVICE: '/order',
     PRODUCT_SERVICE: '/product',
     CATEGORY_SERVICE: '/category-service',
