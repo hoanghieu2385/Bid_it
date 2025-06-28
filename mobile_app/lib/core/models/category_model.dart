751eaa0c5902995fdf91610f9cc558fd62e34ab6
Thanhmai1 <thanhmainguyen20120119@gmail.com>
2025-06-28 09:33:38 +0700

    mobile: fix lỗi icon category

--- mobile_app/lib/core/models/category_model.dart
+++ mobile_app/lib/core/models/category_model.dart
@@ -4,13 +4,15 @@
 class Category {
   final int id;
   final String name;
+  final String? icon;
 
-  Category({required this.id, required this.name});
+  Category({required this.id, required this.name, this.icon});
 
   factory Category.fromJson(Map<String, dynamic> json) {
     return Category(
       id: json['id'],
       name: json['name'],
+      icon: json['icon'],
     );
   }
 
